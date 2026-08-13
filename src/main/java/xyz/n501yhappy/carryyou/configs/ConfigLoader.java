46daaef412696397793ecfa4da4d9211511ed210
n501yhappy <n501yhappy@outlook.com>
2026-08-13 09:40:28 +0800

    默认shift+右键触发

--- src/main/java/xyz/n501yhappy/carryyou/configs/ConfigLoader.java
+++ src/main/java/xyz/n501yhappy/carryyou/configs/ConfigLoader.java
@@ -24,7 +24,7 @@ public class ConfigLoader {
     public static Double THROW_POWER_DROP = 0.5;
     public static Double THROW_POWER_ATTACK = 0.9;
     public static Double THROW_POWER_INTERACT = 0.9;
-    public static Boolean TRIGGER_SHIFT_F = true;
+    public static Boolean TRIGGER_SHIFT_F = false;
     public static Boolean TRIGGER_EMPTY = false;
     public static Boolean WITH_CHICKEN = true;
     public static Boolean WITH_CREEPER = true;
